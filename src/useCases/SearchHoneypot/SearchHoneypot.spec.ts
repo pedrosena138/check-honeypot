51234a887d494fd4ab850a0bd8f4454cff2b5ac4
Pedro Sena <pmfsl@cin.ufpe.br>
2022-02-24 21:39:01 -0300

    fix tests

--- src/useCases/SearchHoneypot/SearchHoneypot.spec.ts
+++ src/useCases/SearchHoneypot/SearchHoneypot.spec.ts
@@ -2,11 +2,16 @@ import { HoneypotService } from "@services/HoneypotService";
 import { SearchHoneypotUseCase } from "./SearchHoneypotUseCase";
 
 describe("SearchHoneypotUseCase", () => {
-  it("should be ok", () => {
-    const service = new HoneypotService();
-    const sut = new SearchHoneypotUseCase(service);
+  let honeypotService: HoneypotService;
+  let sut: SearchHoneypotUseCase;
 
+  beforeEach(() => {
+    honeypotService = new HoneypotService();
+    sut = new SearchHoneypotUseCase(honeypotService);
+  });
+
+  it("should be defined", () => {
     expect(sut).toBeDefined();
-    expect(service).toBeDefined();
+    expect(honeypotService).toBeDefined();
   });
 });
